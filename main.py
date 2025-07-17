dbb8510a5fb26c5a22318753d821d3cb4e339dcb
Kaden Lee <mrkaboommc@gmail.com>
2025-07-18 07:38:24 +1000

    Added main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# 2025 133cx-MRKABOOM
+#
+# SEARCHR
+
+print("2025 133cx-MRKABOOM")
+
+print("SEARCHR")
+
+___main___ = "___main___"
+
+
+#main Funcution
+
+while ___main___ == "___main___":
+    #options
+
+    choice = Null
+
+    choice = input("Browse or Scrape: (B/S)")
+
+    if choice == "B" or "b":
+        #code for browsing
+
+        print("Entered Browsing Mode")
+
+
+    if choice == "S" or "s":
+        # code for scraping
+
+        print("Entered Scraping mode")
+
+    else:
+
+        print("Please Put Valid Option")
