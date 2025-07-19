d2dd75a7b8ed73017f097e3d46a57786e15a1547
Kaden Lee <mrkaboommc@gmail.com>
2025-07-19 10:28:37 +1000

    added scrape.py

---
+++ scripts/scrape.py
@@ -0,0 +1,16 @@
+#Scrape.py
+# 2025 133cx-MRKABOOM
+#
+# Part of the Searchr Project
+
+
+print("Scrape.py running")
+
+___main___ = "___main___"
+
+#main funcution
+
+
+while ___main___ == "___main___":
+
+    #scraping funcunality
