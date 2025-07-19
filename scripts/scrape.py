58a1e895c3cba1007d96c4f92a30a4d21ca7bb03
Kaden Lee <mrkaboommc@gmail.com>
2025-07-19 10:39:23 +1000

    added basic scraping to scrape.py

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -4,13 +4,68 @@
 # Part of the Searchr Project
 
 
+import requests
+from bs4 import BeautifulSoup
+
 print("Scrape.py running")
 
 ___main___ = "___main___"
 
-#main funcution
+#main funcutions
 
 
 while ___main___ == "___main___":
 
-    #scraping funcunality
+    url = Null
+
+
+    # Open the file in read mode
+    file = open("geeks.txt", "r")
+
+    # Read the first line
+    line = file.readline()
+
+    while line:
+
+
+        #prints the url to scrape from file
+        print(line.strip())
+
+
+        #reads one url from the file
+        url = file.readline()
+
+
+        #fetches the data from that url
+        response = requests.get(url)
+
+
+        #loads bs4
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+
+        #finds all <a> tags in the html
+        for link in soup.find_all('a'):
+
+
+            #prints the <a> tag
+            print(link.get('href'))
+
+
+            #puts the <a> tag in href varable
+            href = link.get('href')
+
+
+            #opens /data/urls.txt
+            with open("/../data/urls.txt", "w") as f:
+
+
+                #writes the a tag to /data/urls.txt
+                f.write(href)
+
+            #closes the file
+            f.close()
+
+
+    # Close the file
+    file.close()
